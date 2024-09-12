18752e06a95fea9258b5b7f522a9e50363bcb0da
colll78 <philipdisarro@gmail.com>
2024-09-12 11:42:13 -0700

    boilerplate

---
+++ app/Main.hs
@@ -0,0 +1,104 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+module Main (main) where
+
+import Cardano.Binary qualified as CBOR
+import Data.Aeson (KeyValue ((.=)), object)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Bifunctor (
+  first,
+ )
+import Data.ByteString.Base16 qualified as Base16
+import Data.ByteString.Lazy qualified as LBS
+import Data.Default (def)
+import Data.Text (
+  Text,
+  pack,
+ )
+import Data.Text.Encoding qualified as Text
+import Plutarch (
+  Config (Config),
+  TracingMode (DoTracing, NoTracing, DoTracingAndBinds),
+  compile,
+ )
+import Plutarch.Evaluate (
+  evalScript,
+ )
+import "liqwid-plutarch-extra" Plutarch.Extra.Script (
+  applyArguments,
+ )
+import Plutarch.Prelude
+import Plutarch.Script (Script, serialiseScript)
+import PlutusLedgerApi.V2 (
+  Data,
+  ExBudget,
+ )
+import Ply.Plutarch (
+  writeTypedScript,
+ )
+import PriceDiscoveryEvent.Mint.Standard (mkDiscoveryNodeMPW)
+import PriceDiscoveryEvent.MultiFold (pfoldValidatorW, pmintFoldPolicyW, pmintRewardFoldPolicyW, prewardFoldValidatorW)
+import PriceDiscoveryEvent.Validator (pDiscoverySetValidator, pDiscoverGlobalLogicW)
+import PriceDiscoveryEvent.ProjectTokenHolder (pprojectTokenHolder, pmintProjectTokenHolder)
+import PriceDiscoveryEvent.MultiFoldLiquidity qualified as LiquidityFold 
+import LiquidityEvent.Mint.Standard (mkLiquidityNodeMPW)
+import LiquidityEvent.Validator (pLiquiditySetValidator, pLiquidityGlobalLogicW)
+import LiquidityEvent.ProxyTokenHolderV1 qualified as ProxyTokenHolderV1
+import AlwaysFails (pAlwaysFails, pAuthMint)
+import System.IO
+import LiquidityEvent.LiquidityTokenHolder (pmintLiquidityTokenHolder, pliquidityTokenHolder)
+
+encodeSerialiseCBOR :: Script -> Text
+encodeSerialiseCBOR = Text.decodeUtf8 . Base16.encode . CBOR.serialize' . serialiseScript
+
+evalT :: Config -> ClosedTerm a -> Either Text (Script, ExBudget, [Text])
+evalT cfg x = evalWithArgsT cfg x []
+
+evalWithArgsT :: Config -> ClosedTerm a -> [Data] -> Either Text (Script, ExBudget, [Text])
+evalWithArgsT cfg x args = do
+  cmp <- compile cfg x
+  let (escr, budg, trc) = evalScript $ applyArguments cmp args
+  scr <- first (pack . show) escr
+  pure (scr, budg, trc)
+
+-- writePlutusScript :: String -> FilePath -> ClosedTerm a -> IO ()
+-- writePlutusScript title filepath term = do
+--   case evalT term of
+--     Left e -> putStrLn (show e)
+--     Right (script, _, _) -> do
+--       let
+--         scriptType = "PlutusScriptV2" :: String
+--         plutusJson = object ["type" .= scriptType, "description" .= title, "cborHex" .= encodeSerialiseCBOR script]
+--         content = encodePretty plutusJson
+--       LBS.writeFile filepath content
+
+writePlutusScript :: Config -> String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScript cfg title filepath term = do
+  case evalT cfg term of
+    Left e -> putStrLn (show e)
+    Right (script, _, _) -> do
+      let
+        scriptType = "PlutusScriptV2" :: String
+        plutusJson = object ["type" .= scriptType, "description" .= title, "cborHex" .= encodeSerialiseCBOR script]
+        content = encodePretty plutusJson
+      LBS.writeFile filepath content
+
+writePlutusScriptTraceBind :: String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScriptTraceBind title filepath term =
+  writePlutusScript (Config DoTracingAndBinds) title filepath term
+
+writePlutusScriptTrace :: String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScriptTrace title filepath term =
+  writePlutusScript (Config DoTracing) title filepath term
+
+writePlutusScriptNoTrace :: String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScriptNoTrace title filepath term =
+  writePlutusScript (Config NoTracing) title filepath term
+
+main :: IO ()
+main = do
+  putStrLn "Writing Plutus Scripts to files"
+  -- writePlutusScriptNoTrace "Discovery Stake Validator" "./compiled/discoveryStakeValidator.json" pDiscoverGlobalLogicW
+  -- writePlutusScriptTraceBind "Liquidity Stake Validator" "./compiledLiquidityBinds/liquidityStakeValidator.json" pLiquidityGlobalLogicW
+  -- writePlutusScriptTrace "Liquidity Token Holder Validator" "./compiledLiquidityTracing/liquidityTokenHolderValidator.json" pliquidityTokenHolder
