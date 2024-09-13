062da5bd29a5aa688ef3d6bfed4f52b2e471cb5b
colll78 <philipdisarro@gmail.com>
2024-09-13 16:14:32 -0700

    fix

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -4,6 +4,18 @@ let
 in
 [
   (
-    project.flake
+    project.flake // {
+      checks = {
+        plutarch-import-test = pkgs.runCommand "plutarch-import-test" {} ''
+          if [ -d ${pkgs.plutarch} ]; then
+            echo "Plutarch found at ${pkgs.plutarch}"
+            touch $out
+          else
+            echo "Plutarch not found"
+            exit 1
+          fi
+        '';
+      };
+    }
   )
 ]
--- nix/project.nix
+++ nix/project.nix
@@ -4,23 +4,28 @@ let
   sha256map = {
     "https://github.com/j-mueller/sc-tools"."e1f4359b6fb6a2b939bac0840e229193dd4f6b54" = "sha256-+dVtHGOQHvAjwOZZ0ZyHsQLF2roREUVIoMU31AD3YZs=";
     "https://github.com/Plutonomicon/plutarch-plutus"."e50661e24670974b398be19426617bc6389fdac6" = "f7dd88e3664a72dacc91f488b122955a874a6615d88961c9e87f2c93bf833f86";
-
   };
 
-
-  modules = [
-  ];
-
+  modules = [{
+    packages = {
+      plutarch = {
+        src = pkgs.plutarch;
+        flags = {};
+      };
+    };
+  }];
 
   cabalProject = pkgs.haskell-nix.cabalProject' {
     inherit modules sha256map;
     src = ../.;
     name = "proposal-loans";
     compiler-nix-name = "ghc966";
-    inputMap = { "https://chap.intersectmbo.org/" = inputs.CHaP; };
+    inputMap = { 
+      "https://chap.intersectmbo.org/" = inputs.CHaP;
+      "https://github.com/Plutonomicon/plutarch-plutus" = pkgs.plutarch;
+    };
     shell.withHoogle = false;
   };
-
 
   project = lib.iogx.mkHaskellProject {
     inherit cabalProject;
@@ -28,5 +33,4 @@ let
   };
 
 in
-
 project
